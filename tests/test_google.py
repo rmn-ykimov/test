6c9a36cfe19bce473342226f872e0e062c72791e
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2023-01-30 13:37:19 +0400

    Changing tests structure

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,14 +1,44 @@
+"""
+This module contains a functional test for searching on Google.
+
+The test navigates to Google, enters a search query, submits the form,
+locates the search results, and asserts that there are more than 10 results
+and that one of the results contains the string "mvideo.ru".
+"""
 from config import GOOGLE_URL
 
 def test_google_search(browser_firefox):
+    """
+    Function to test searching on Google using the browser_firefox object.
+
+    Args:
+    browser_firefox (obj): A Selenium webdriver object to interact with the
+    browser
+
+    Returns:
+    None
+
+    Raises:
+    AssertionError: If the number of search results is not greater than 10 or
+    if the search results do not contain the string "mvideo.ru"
+    """
+    # Navigate to Google URL
     browser_firefox.get(GOOGLE_URL)
 
+    # Locate the search box
     search_box = browser_firefox.find_element_by_name("q")
-    search_box.send_keys("buy a bork c804 coffee machine")
+
+    # Enter the search query into the search box
+    search_box.send_keys("купить кофемашину bork c804")
+
+    # Submit the form
     search_box.submit()
 
+    # Locate the search results
     results = browser_firefox.find_elements_by_css_selector(".g")
-    assert len(results) > 10, f"Expected more than 10 results, but got {len(results)}"
-    assert any("mvideo.ru" in result.text for result in results), "mvideo.ru not found in search results"
 
-#2. Write an autotest for Google search. The words “buy a bork c804 coffee machine” are entered into the search line, there are more than 10 results and mvideo.ru is present in the search results.
+    # Assert that there are more than 10 results
+    assert len(results) > 10, f"Expected more than 10 results, but got {len(results)}"
+
+    # Assert that the results contain the string "mvideo.ru"
+    assert any("mvideo.ru" in result.text for result in results), "mvideo.ru not found in search results"
--- tests/test_login_to_yandex_mail.py
+++ tests/test_login_to_yandex_mail.py
@@ -1,8 +1,22 @@
+"""
+This module tests the login functionality of Yandex Mail by testing different
+scenarios for the username and password input fields.
+The scenarios include providing valid credentials, invalid credentials, missing
+username and missing password.
+The test uses the pytest framework, Selenium WebDriver and the
+expected_conditions module to interact with the browser and verify the result.
+
+@pytest.mark.xfail: This test is expected to fail.
+@pytest.mark.parametrize: This decorator is used to run the same test with
+multiple sets of inputs, defined in the list of tuples.
+browser_firefox: This is the fixture that provides the instance of the Firefox
+browser.
+YANDEX_URL: This is the URL of Yandex Mail.
+"""
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
 from config import YANDEX_URL
 
 @pytest.mark.xfail
@@ -14,6 +28,23 @@ from config import YANDEX_URL
     ("valid_user", "missing_password", "Password is required")
 ])
 def test_login_to_yandex_mail(browser_firefox, username, password, expected_result):
+    """
+    This function tests the login functionality of Yandex Mail by testing
+    different scenarios for the username and password input fields.
+
+    Arguments:
+    browser_firefox: instance of the Firefox browser, provided by a fixture.
+    username: the username to be entered in the login form.
+    password: the password to be entered in the login form.
+    expected_result: the expected result of the login action, can be either
+    "Logged in successfully", "Invalid credentials", "Username is required", or
+    "Password is required".
+
+    The function opens the Yandex Mail URL, enters the username and password,
+    submits the form, and verifies the result based on the expected result. If
+    the result is "Logged in successfully", the function also logs out the
+    account.
+    """
     browser_firefox.get(YANDEX_URL)
     # enter username and password
     username_input = browser_firefox.find_element_by_id("passp-field-login")
@@ -39,8 +70,3 @@ def test_login_to_yandex_mail(browser_firefox, username, password, expected_resu
         logout_link.click()
     else:
         assert expected_result in browser_firefox.page_source
-
-
-
-
-#1. Write an autotest to enter the mailbox on www.yandex.ru.
